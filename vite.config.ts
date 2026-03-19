7e6ef6df678be323dabf38ee4886e60fbc466664
krwakk <yuliy14052010@gmail.com>
2026-03-19 13:30:56 -0300

    Remove vite-plugin-singlefile para corrigir deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      // Esta é a forma mais moderna de fazer o alias do "@" no Vite (ESM)
+      // Forma moderna de fazer o alias do "@" no Vite (ESM)
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
